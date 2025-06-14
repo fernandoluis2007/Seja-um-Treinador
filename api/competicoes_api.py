189233f800ec2cab2261e8d068290da6275cbcbc
fernandoluis2007 <luisfernando0uti45@gmail.com>
2025-06-13 21:38:49 -0300

    Puxando dados do arquivo .json e manipulando-os - Testes 1

--- api/competicoes_api.py
+++ api/competicoes_api.py
@@ -15,8 +15,21 @@ def obter_times_da_liga(api_token, liga, salvar_em=False):
 
     return times_da_liga
 
+# chamando a função para acessar a API e consumi-lá e armazenar na pasta Data todos os .json de cada liga. 
+'''
 liga_world_cup = obter_times_da_liga(api_token, 'WC')
 liga_champions_league = obter_times_da_liga(api_token, 'CL')
+liga_aleman = obter_times_da_liga(api_token, 'BL1')
+liga_horlandesa = obter_times_da_liga(api_token, 'DED')
+liga_brasileira = obter_times_da_liga(api_token, 'BSA')
+liga_espanhola = obter_times_da_liga(api_token, 'PD')
+liga_francesa = obter_times_da_liga(api_token, 'FL1')
+liga_inglesa_segunda_divisao = obter_times_da_liga(api_token, 'ELC')
+liga_portuguesa = obter_times_da_liga(api_token, 'PPL')
+liga_euro_copa = obter_times_da_liga(api_token, 'EC')
+liga_italiana = obter_times_da_liga(api_token, 'SA')
+liga_inglesa_primeira_divisao = obter_times_da_liga(api_token, 'PL')
+'''
 
 
 
--- api/times_api.py
+++ api/times_api.py
@@ -5,7 +5,51 @@ import json
 
 # Importar a função base request para consumir API.
 from resquest_football_data import request_api, chamar_api_key
+'''
 
 api_token= chamar_api_key()
 dados = request_api(api_token, 'teams')
 
+'''
+# Ler o arquivo .json ->
+with open('../data/competitions/BSA.json', 'r', encoding='utf-8') as file:
+    times_brasileiro = json.load(file)
+    for x in times_brasileiro["seasons"]:
+        if x["winner"] != None:
+            dados = x["winner"]
+            for time in dados:
+                print(dados["id"], dados["tla"], dados["name"])
+        
+
+#
+#for time in times_brasileiro['']
+#           print(f'Nome Clube: {times_brasileiro["name"]}\n Id do clube: {times_brasileiro["id"]}')
+#
+
+            
+
+
+
+
+# Armazenar os arquvios lidos do .json ->
+def armazenar_times_lista(id_time, nome, tla, fundado_em, estadio, endereco):
+    dicionario_times_brasileiro = {
+        'id': [id_time],
+        'nome': [nome],
+        'sigla': [tla],
+        'fundado': [fundado_em],
+        'estadio': [estadio],   
+        'endereco': [endereco]
+    }
+
+    # f-strings, para retornar os dados do time ->
+    mensagem = (
+        f'Id do clube: {id_time}\n'
+        f'Nome do clube: {nome}\n'
+        f'Sigla do clube: {tla}\n'
+        f'Clube fundado em: {fundado_em}\n'
+        f'Estádio do clube: {estadio}\n'
+        f'Endereço do Clube: {endereco}\n'
+    )
+
+    return mensagem
