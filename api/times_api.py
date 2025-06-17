3855264454d83792c7c63daff8008659bd09b010
fernandoluis2007 <luisfernando0uti45@gmail.com>
2025-06-17 20:46:02 -0300

    Refatorando a função para puxar nome dos times da API

--- api/times_api.py
+++ api/times_api.py
@@ -8,22 +8,39 @@ from resquest_football_data import request_api, chamar_api_key
 
 '''
 api_token= chamar_api_key()
-dados = request_api(api_token, 'competitions/BSA/teams', False)
+dados = request_api(api_token, 'competitions/WC/teams', False)
 '''
 
+# Função ler o arquivo .json -> e armazenar os times lidos dentro de uma lista:
+def puxar_nome_times(pasta, arquivo): # O parâmetro é o nome do arquvio /-> ex: PL, WC, SA...
+    nome_pasta = pasta
+    nome_liga = arquivo
+    lista_completa_times_liga = []
 
-# Armazenar os nomes dos times, dos arquvios lidos do .json ->
-lista_completa_times_br = []
+    with open(f'../data/competitions/{nome_pasta}/{nome_liga}.json', 'r', encoding='utf-8') as file:
+        lista_times = json.load(file)
+        try:
+            for x in lista_times["teams"]: # Chave de acesso: ["teams"].
+                lista_completa_times_liga.append(x["name"])
+        except KeyError: # Quando tenta acessar uma chave inexistente em um dicionário.
+            print("A chave 'teams' não existe no arquivo!")
 
-# Ler o arquivo .json -> e armazenar os times lidos dentro de uma lista:
-with open('../data/competitions/teams.json', 'r', encoding='utf-8') as file:
-    times_brasileiro = json.load(file)
-    lista_completa_times_br = []
-    for x in times_brasileiro["teams"]: # Chave de acesso: ["teams"].
-        lista_completa_times_br.append(x["name"])
-        
-print(lista_completa_times_br)
-# Teste --> print(lista_completa_times_br).
+    return lista_completa_times_liga
+           
+times_brasileirao = puxar_nome_times("BSA","teams")
+times_bundelisga = puxar_nome_times("BL1", "teams")
+times_champions_league = puxar_nome_times("CL", "teams")
+times_eredivisie = puxar_nome_times("DED", "teams")
+times_european_championship = puxar_nome_times("EC", "teams")
+times_championship = puxar_nome_times("EC", "teams")
+times_francesa = puxar_nome_times("FL1", "teams")
+times_espanhola = puxar_nome_times("PD", "teams")
+times_premier_league = puxar_nome_times("PL", "teams")
+times_portuguesa = puxar_nome_times("PPL", "teams")
+times_italiana = puxar_nome_times("SA", "teams")
+selecoes = puxar_nome_times("WC", "teams")
 
-# lista_completa_times_br.append(x["name"])
-       
+print(times_brasileirao)
+print(selecoes)
+
+
